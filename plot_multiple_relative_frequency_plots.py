702ce0196b2facfc0998c57c7f647e1f9cb7c202
Denis Bozic <45030572+dbozic@users.noreply.github.com>
2020-09-22 17:13:37 -0400

    plot multiple relative frequency plots

---
+++ plot_multiple_relative_frequency_plots.py
@@ -0,0 +1,42 @@
+def plot_multiple_relative_frequency_plots(data, variables, grouped_variable, figsize, legend, color, title, fontsize, tickfontsize, weight,
+    xfontsize, xweight, ytitle, yfontsize, yweight, xrotation, sort, top):
+  
+ """
+ This function uses the relative_frequency_plot and automates plotting of multiple charts
+ when you need to visualize relative frequency plots for a bunch of variables. You will 
+ use the same arguments as relative_frequency_plot, and there will be new ones this time:
+ 
+ INPUTS: 
+ 
+ variables: a list of strings, where each string denotes the name of the column/variable
+  in the pandas dataframe you want to visualize
+
+ OUTPUT: 
+ 
+ multiple relative frequency plots
+ 
+ """
+ for variable in variables:
+   
+  relative_frequency_plot(
+    data = data, 
+    group_class = variable,
+    grouped_variable = grouped_variable,
+    figsize = figsize,
+    legend = legend, 
+    color = color, 
+    title = title, 
+    fontsize = fontsize, 
+    tickfontsize = tickfontsize, 
+    weight = weight,
+    xtitle = variable, 
+    xfontsize = xfontsize, 
+    xweight = xweight, 
+    ytitle = ytitle, 
+    yfontsize = yfontsize, 
+    yweight = yweight, 
+    xrotation = xrotation, 
+    sort = sort, 
+    top = top
+    
+  )
